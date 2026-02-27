2145ecdc72601b8793401ef678d5beb2fc65ef89
JesseKimani <jessekimani21@gmail.com>
2026-02-27 20:41:38 +0300

    fix build error

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('PorfolioWebsite');
+  protected readonly title = signal('PortfolioWebsite');
 }
